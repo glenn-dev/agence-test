acc0d6cf270210422fea6f3decb15b83b599900b
glenn-dev <glenn.marcano90@gmail.com>
2020-04-19 01:04:29 -0400

    Index view added, navbar fixed

--- app/controllers/performance_controller.rb
+++ app/controllers/performance_controller.rb
@@ -2,8 +2,8 @@ class PerformanceController < ApplicationController
 
   
   def index
-    results = Performance.monthly_report_by_user
-    render json: results.to_json
+    @reports = Performance.monthly_report_by_user
+    #render json: @reports.to_json
   end
 
 end
