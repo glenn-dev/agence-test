3e552b0cbb2166b51646ecd9bdd251eabb60380e
glenn-dev <glenn.marcano90@gmail.com>
2020-04-19 22:40:07 -0400

    View index fixed, shows relatorio

--- app/controllers/performance_controller.rb
+++ app/controllers/performance_controller.rb
@@ -1,9 +1,30 @@
 class PerformanceController < ApplicationController
-
+  before_action :set_reports, only: [:relatorio, :grafico, :pizza]
   
   def index
-    @reports = Performance.monthly_report_by_user
+    @users = Performance.get_users
     #render json: @reports.to_json
   end
+
+  def relatorio
+  end
+
+  def grafico
+  end
+
+  def pizza
+  end
+
+  private
+
+  def set_reports
+    @reports = Performance.monthly_report_by_user
+    # (params[:users, :date1, :date2])
+  end
+  # @communication = Communication.find(params[:id])
+
+  # def reports_params
+  #   params.require(:communication).permit(:users, :date1, :date2)
+  # end
 
 end
